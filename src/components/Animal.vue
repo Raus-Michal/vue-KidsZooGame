4efa4033dd0854eb144434b4f9747a4746c32203
Michal Raus <raus.michal@email.cz>
2025-03-17 11:02:05 +0100

    Vycisteni projektu

---
+++ src/components/Animal.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<p>Já jsem zvíře</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<p>Já jsem hlavička</p>
+</template>
+
+<style scoped>
+
+</style>
