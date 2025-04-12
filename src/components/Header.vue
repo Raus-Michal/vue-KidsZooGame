d4a43cd539c22cba07bb0cbb645a3a6f4b2bf599
Michal Raus <raus.michal@email.cz>
2025-04-12 19:55:01 +0200

    Pridani logoky otevirani dialogoveho okna pri kliknuti na logo aplikace

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { ref } from "vue";
 
 const changeColor=(color:string)=>{
 // funkce přebarví pozadí aplikace
@@ -13,12 +13,46 @@ document.body.style.backgroundColor=color; // změní barvu pozadí těla Vue ap
 
 };
 
+
+const dialog = ref<HTMLDialogElement | null>(null); // Reference na dialogový element
+
+// Funkce pro otevření dialogu
+  const openDialog = (event: Event) => {
+    event.preventDefault(); // Zabrání přesměrování nebo akci odkazu
+    if (dialog.value !== null) {
+      // pokud HTML element existuje
+      dialog.value.showModal();  // Zobrazí dialog
+    }
+  };
+
+// Funkce pro zavření dialogu
+  const closeDialog = () => {
+    if (dialog.value !== null) {
+      // pokud HTML element existuje
+      dialog.value.close();  // Zavře dialog
+    }
+  };
+
+// funkce zavře dialog, pokud uživatel klik mimo dialogové okno
+  const closeDialogOnBackdropClick = (event: MouseEvent) => {
+    if (event.target === dialog.value) {
+        // event.target – prvek, na který uživatel skutečně kliknul. dialog.value – reference na <dialog> (tedy samotný dialogový prvek). Když je splněna podmínka, znamená to, že uživatel klikl na pozadí dialogu – to můžeš využít k jeho zavření. Když nejsou stejné (false), znamená to, že uživatel klikl na něco uvnitř dialogu (např. tlačítko), takže nechceš zavřít okno.
+        dialog.value?.close(); // Co znamená dialog.value?.close();? Pokud dialog.value existuje (není null nebo undefined), zavolá se close(). Pokud dialog.value neexistuje, nic se nestane – skript nevyhodí chybu.
+    }
+};
+
 </script>
 
 <template>
 
+<dialog ref="dialog" @click="closeDialogOnBackdropClick">
+    <p>&copy;&#8239;2024 Boar-cz</p>
+    <p>(všechna práva vyhrazena)</p>
+    <button @click="closeDialog" type="button" title="close">Close dialog</button>
+</dialog>
+
 <header>
-    <h1>ZOO Game</h1>
+    <a href="#" @click="openDialog" title="Information" ><h1>ZOO Game</h1></a>
     <aside>
         <button type="button" title="Black color" @click="changeColor('black')"></button>
         <button type="button" title="Royalblue color" @click="changeColor('royalblue')"></button>
@@ -71,6 +105,26 @@ background-color:black;
 aside button:last-child
 {
 background-color:rebeccapurple;
+}
+
+a,a:link,a:visited,a:active
+{
+color:var(--b1);
+text-decoration:none;
+}
+
+
+dialog {
+  border:none;
+  padding:1rem;
+  background-color:black;
+  color:white;
+  text-align:center;
+  border-radius:16px;
+}
+
+dialog::backdrop {
+background-color: rgba(80, 28, 28, 0.25); /* Poloprůhledné pozadí pro zbytek stránky */
 }
 
 </style>
