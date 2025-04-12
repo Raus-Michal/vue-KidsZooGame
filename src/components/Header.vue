af8c52568d74fcfbc9236efe44eb6f83faf63adc
Michal Raus <raus.michal@email.cz>
2025-04-12 19:50:56 +0200

    Pridani moznosti pro uzivatele: zmeni barvu pozadi aplikace

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
 
+
+const changeColor=(color:string)=>{
+// funkce přebarví pozadí aplikace
+const app=document.getElementById("app") as HTMLElement; // tělo Vue aplikace
+
+if(app) {
+// pokud HTML element existuje
+app.style.backgroundColor=color; // změní barvu pozadí těla Vue aplikace
+document.body.style.backgroundColor=color; // změní barvu pozadí těla Vue aplikace
+}
+
+};
+
 </script>
 
 <template>
 
 <header>
     <h1>ZOO Game</h1>
+    <aside>
+        <button type="button" title="Black color" @click="changeColor('black')"></button>
+        <button type="button" title="Royalblue color" @click="changeColor('royalblue')"></button>
+        <button type="button" title="Rebeccapurple color" @click="changeColor('rebeccapurple')"></button>
+    </aside>
 </header>
 
 </template>
 
 <style scoped>
+
 header
 {
-margin-left:1rem;
-margin-top:1rem;
+display: flex;
+align-items:center;
+gap:2rem;
+flex-wrap:wrap;
+margin:1rem 4.5rem 0 1rem; /* margin-right 4.5rem je místo pro button SoundControler */
 }
 
 header h1
@@ -24,5 +46,31 @@ margin:0;
 padding:0;
 }
 
+aside
+{
+display:flex;
+gap:1.5rem;
+}
+
+aside button
+{
+min-height:2rem;
+height:2rem;
+min-width:2rem;
+width:2rem;
+border-radius:50%;
+border:2px solid white;
+background-color:royalblue;
+}
+
+aside button:first-child
+{
+background-color:black;
+}
+
+aside button:last-child
+{
+background-color:rebeccapurple;
+}
 
 </style>
