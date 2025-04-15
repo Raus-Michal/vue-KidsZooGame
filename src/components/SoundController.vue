7121aaae2621adfae27dd084679c5cc3a55393e9
Michal Raus <raus.michal@email.cz>
2025-04-15 18:08:21 +0200

    Zmena ikon v SoundController

--- src/components/SoundController.vue
+++ src/components/SoundController.vue
@@ -8,22 +8,22 @@ const animals = useAnimals(); // Použití store Pinia v proměnné animals
 <template>
 <div class="container">
     <button v-if="animals.sound_play" type="button" title="Zastav zvuk" @click="animals.soundAnimalstop">
-        <svg class="svg-sound" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-            <path class="fill-white" d="M 41.5,90 40.76,12.8 23.94,38.36 5.35,38.73 v 27.66 l 18.88,1.57 z" />
-            <g class="stroke-5">
-                <line x1="55" y1="35" x2="90" y2="70" />
-                <line x2="55" y1="35" x1="90" y2="70" />
-            </g>
-        </svg>
-    </button>
-
-    <button v-else-if="!animals.sound_play" type="button" title="Pustit zvuk" @click="animals.soundAnimalplay" >
         <svg class="svg-sound" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
             <path class="fill-white" d="m 41.5,88.7 -1,-77.2 -16.8,25.6 -18.6,0.5 v 27.7 l 18.9,1.6 z" />
             <g class="stroke-5">
                 <path id="s1" class="anim-stroke" d="m 50.9,38.6 a 8.7,12.8 0 0 1 7.6,6.3 8.7,12.8 0 0 1 0.1,12.8 8.7,12.8 0 0 1 -7.6,6.4" />
                 <path id="s2" class="anim-stroke" d="M 63,28.6 A 12.7,22.6 0 0 1 74.2,39.3 12.7,22.6 0 0 1 74.3,62 12.7,22.6 0 0 1 63.2,73.4" />
                 <path id="s3" class="anim-stroke" d="m 75.8,15.6 a 16.9,33.5 0 0 1 14.7,17.2 16.9,33.5 0 0 1 0,33.5 16.9,33.5 0 0 1 -14.7,17.2" />
+            </g>
+        </svg>
+    </button>
+
+    <button v-else-if="!animals.sound_play" type="button" title="Pustit zvuk" @click="animals.soundAnimalplay" >
+        <svg class="svg-sound" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <path class="fill-white" d="M 41.5,90 40.76,12.8 23.94,38.36 5.35,38.73 v 27.66 l 18.88,1.57 z" />
+            <g class="stroke-5">
+                <line x1="55" y1="35" x2="90" y2="70" />
+                <line x2="55" y1="35" x1="90" y2="70" />
             </g>
         </svg>
     </button>
